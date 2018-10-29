992e574c741ababf5d6574627c53125dfb1ee380
Michael Spraggins <michael.c.spraggins@gmail.com>
2018-10-29 13:50:18 -0700

    Added dev dependencies for ease of development.

---
+++ bundle.js
@@ -0,0 +1,1 @@
+console.error("Error: Cannot find module '/Users/michaelspraggins/quarterTwoLessons/ecommerce-project/scripts/main.js' from '/Users/michaelspraggins/quarterTwoLessons/ecommerce-project'");
