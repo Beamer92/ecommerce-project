d220ccae29ded63335ecc1ab396daee69be6cc89
Montijello <32350934+Montijello@users.noreply.github.com>
2018-11-06 14:28:20 -0800

    added main page

---
+++ bundle.js
@@ -0,0 +1,1 @@
+console.error("Error: Cannot find module '/home/jello/galvanize/projects/ecommerce-project/scripts/main.js' from '/home/jello/galvanize/projects/ecommerce-project'");
---
+++ js/index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function(e) {
+
+  if (localStorage.getItem('cartCount') === null) {
+    localStorage.setItem('cartCount', 0)
+  }
+  else {
+    level = parseInt(localStorage.getItem('cartCount'))
+  }
+
+  if (localStorage.getItem('cartItems') === null) {
+    localStorage.setItem('cartItems', {})
+  }
+  else {
+    level = parseInt(localStorage.getItem('cartCount'))
+  }
+
+})
