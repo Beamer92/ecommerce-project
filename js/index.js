26825f12aeb789aa19b117f054be9e635daf47e3
Brendan Woodell <brendan.woodell@gmail.com>
2018-11-09 10:42:34 -0800

    JS is life

--- js/index.js
+++ js/index.js
@@ -1,69 +1,112 @@
-document.querySelector('#emailSignup').addEventListener('submit', function(event){
+const products = require(`./productsDatabase`)
+
+let cartItems = {}
+const sCart = document.getElementById("navbarDropdown2")
+const sCartdd = document.getElementById("ddMenuCart")
+const clrCart = document.getElementById("clrCart")
+const esignup = document.querySelector('#emailSignup')
+const termspriv = document.querySelector('#termsAndPriv')
+
+if(esignup) {
+    esignup.addEventListener('submit', function(event){
+      event.preventDefault()
+      document.querySelector('#emailSignup').reset()
+      alert('Thank you for signing up!')
+  })
+}
+
+if(termspriv){
+  termspriv.addEventListener('click', function(event){
     event.preventDefault()
-    document.querySelector('#emailSignup').reset()
-    alert('Thank you for signing up!')
-})
+    alert('So much spam')
+  })
+}
 
-document.querySelector('#termsAndPriv').addEventListener('click', function(event){
-  event.preventDefault()
-  alert('So much spam')
-})
+if(clrCart) {
+  clrCart.addEventListener("click", function(e) {
+    localStorage.clear()
+  })
+}
 
-  let cartItems = {}
-  const sCart = document.getElementById("navbarDropdown2")
-  const sCartdd = document.getElementById("ddMenuCart")
+function tTotals(obj) {
+  let price = 0
+  let count = 0
 
-  // if (localStorage.getItem('cartCount') === null) {
-  //   cCount = localStorage.setItem('cartCount', 1)
-  // }
-  // else {
-  //   cCount = parseInt(localStorage.getItem('cartCount'))
-  // }
+  for(let i in obj){
+    price += (obj[i].Qty * obj[i].Price)
+    count ++
+  }
+  return [price, count]
+}
 
-  if (localStorage.getItem('cartItems') === null) {
-      localStorage.setItem('cartItems', JSON.stringify({
-      NewCastle: {name: "NewCastle", Qty: 4, Price: 30},
-      Razor: {name: "Razor", Qty:1, Price: 165}
-    }))
-
-   cartItems = JSON.parse(localStorage.getItem('cartItems'))
+function setCart(arr) {
+  if(arr[1] === 0) {
+    sCart.innerHTML = "Shopping Cart"
   }
   else {
-   cartItems = JSON.parse(localStorage.getItem('cartItems'))
+    sCart.innerHTML = `Shopping Cart (${arr[1]})  Total: $${arr[0]}`
   }
+}
 
-  function tTotals(obj) {
-    let price = 0
-    let count = 0
-  
-    for(let i in obj){
-      price += (obj[i].Qty * obj[i].Price)
-      count ++
-    }
-    return [price, count]
+function createCartdd(obj) {
+  for(let i in obj) {
+    let newItem = document.createElement("a")
+    newItem.className = "dropdown-item"
+    newItem.href="#"
+    newItem.innerHTML= `${obj[i].name} : $${(obj[i].Qty) * (obj[i].Price)}`
+
+    sCartdd.insertBefore(newItem, sCartdd.firstChild)
   }
-  
-  function setCart(arr) {
-    if(arr[1] === 0) {
-      sCart.innerHTML = "Shopping Cart"
-    }
-    else {
-      sCart.innerHTML = `Shopping Cart (${arr[1]})  Total: $${arr[0]}`
-    }
-  }
-  
-  function createCartdd(obj) {
-    for(let i in obj) {
-      let newItem = document.createElement("a")
-      newItem.className = "dropdown-item"
-      newItem.href="#"
-      newItem.innerHTML= `${obj[i].name} : $${(obj[i].Qty) * (obj[i].Price)}`
-  
-      sCartdd.insertBefore(newItem, sCartdd.firstChild)
-    }
-  }
+}
+
+const addClickFunctionality = () => {
+    const querySelectors =[]
+    let a = 150
+    let i=0
+
+    while(a) {
+        a = document.getElementById(i)
+        if(a) querySelectors.push(a)
+        i++
+      }
+
+    console.log(querySelectors)
+
+    querySelectors.map((ele) => {
+        let product
+        for(let i = 0; i < products.length; i++){
+            if(ele.classList[1] === products[i].key) {
+                product = products[i]
+            }
+        }
+
+        ele.addEventListener('click', (e) => {
+            e.preventDefault()
+            // console.log(product)
+            cartItems = JSON.parse(localStorage.getItem('cartItems'))
+            // console.log(cartItems)
+            // cartItems.product.key = 
+            //do localstorage stuff from product here name/price
+        })
+    })
+}
+
+addClickFunctionality()
+
+
+  // if (localStorage.getItem('cartItems') === null) {
+  //     localStorage.setItem('cartItems', JSON.stringify({
+  //     NewCastle: {name: "NewCastle", Qty: 4, Price: 30},
+  //     Razor: {name: "Razor", Qty:1, Price: 165}
+  //   }))
+
+  //  cartItems = JSON.parse(localStorage.getItem('cartItems'))
+  // }
+  // else {
+cartItems = JSON.parse(localStorage.getItem('cartItems'))
 
 
 
 setCart(tTotals(cartItems))
 createCartdd(cartItems)
+
